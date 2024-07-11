aeb59f9c259046b4d20f32c36bb18d533eb17754
leandryscsec <leandryscsec@gmail.com>
2024-07-11 15:17:41 -0400

    add basic math functions and basic variables

---
+++ index.js
@@ -0,0 +1,19 @@
+let firstNumber = 0;
+let secondNumber = 0;
+let operator = "";
+
+function add(x, y) {
+    return x + y;
+}
+
+function subtract(x, y) {
+    return x - y;
+}
+
+function multiply(x, y) {
+    return x * y;
+}
+
+function divide(x, y) {
+    return Math.floor(x / y) / 10;
+}
