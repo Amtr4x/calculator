34c7ad54c2600f705aa21f4defe3bd55b001e9f4
leandryscsec <leandryscsec@gmail.com>
2024-07-11 15:30:56 -0400

    Add operate function

--- index.js
+++ index.js
@@ -2,6 +2,37 @@ let firstNumber = 0;
 let secondNumber = 0;
 let operator = "";
 
+/**
+ * Perform the requested operation based in the provided operator.
+ * @param {number} x - first number
+ * @param {string} op - operator
+ * @param {number} y - second number
+ * @returns result of the operation or a string indicating an error.
+ */
+function operate(x, op, y) {
+    let result = 0;
+
+    switch (op) {
+        case "+":
+            result = add(x, y);
+            break;
+        case "-":
+            result = subtract(x, y);
+            break;
+        case "*":
+            result = subtract(x, y);
+            break;
+        case "/":
+            result = divide(x, y);
+            break;
+        default:
+            result = "Invalid Operator";
+            break;
+    }
+
+    return result;
+}
+
 function add(x, y) {
     return x + y;
 }
