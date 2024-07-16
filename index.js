bb0674e6bd05ae783fa08c8c16a5533ac75a447b
leandryscsec <leandryscsec@gmail.com>
2024-07-16 12:04:24 -0400

    implement storeValueFrom function to allocate the numeric values

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 const buttons = document.querySelector(".calculator__buttons");
+let accumulator = "";
+let mutator = "";
+let operator = "";
 
 function manageMouseInput() {
     buttons?.addEventListener("click", (btn) => {
-        console.log(
-            `key pressed = ${btn.target.value} is ${inputType(
-                btn.target.value
-            )}`
-        );
+        const btnPressed = btn.target.value;
+        const inputType = inputTypeOf(btnPressed);
+
+        if (inputType === "number") {
+            storeValueFrom(btnPressed);
+        } else if (inputType === "operator") {
+            if (accumulator.length > 0) {
+                operator += btnPressed;
+            }
+        } else {
+            console.log("this is an special call, must perform an operation");
+        }
     });
 }
 
@@ -17,6 +27,14 @@ function inputTypeOf(x) {
         return "specials";
     } else {
         return "operator";
+    }
+}
+
+function storeValueFrom(x) {
+    if (operator.length === 0) {
+        accumulator += x;
+    } else {
+        mutator += x;
     }
 }
 
