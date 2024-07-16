3f8cb8b0fb41e623b052bb07b1b2564e55737530
leandryscsec <leandryscsec@gmail.com>
2024-07-16 18:06:52 -0400

    Update the conditional logic in mouse events
    This change is in order to prevent the user insert double operators,
    call final results with no requirements met

--- index.js
+++ index.js
@@ -7,15 +7,19 @@ function manageMouseInput() {
     buttons.addEventListener("click", (btn) => {
         const btnPressed = btn.target.value;
         const inputType = inputTypeOf(btnPressed);
+        const requirementsAreReady =
+            accumulator && operator && mutator ? true : false;
 
         if (inputType === "number") {
             storeValueFrom(btnPressed);
         } else if (inputType === "operator") {
             if (accumulator && !operator) {
                 operator += btnPressed;
+            } else if (requirementsAreReady) {
+                // TODO call required operation, then store the result in accumulator, clean mutator, store the new operator and fill mutator
             }
         } else {
-            if (btn === "=" && accumulator && operator && operator) {
+            if (btn === "=" && requirementsAreReady) {
                 // TODO call operations
             }
         }
